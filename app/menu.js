a794ac323a8d3b665b3228c86d633aac7a74329a
Leonardo Munoz <leonardo.munoz@globant.com>
2017-08-15 17:58:41 -0500

    work on menu

--- app/menu.js
+++ app/menu.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
-const Menu = () => {
+
+const Menu = (props) => {
+
+  const isActive = (route) => {
+    return route === props.location.pathname ? 'item--active' : '';
+  }
+
   return (
     <div className="menu">
-      <Link className="item" to="/profile">Profile</Link>
-      <Link className="item" to="/albums">Albums</Link>
-      <Link className="item" to="/my-top">My Top</Link>
-      <Link className="item" to="/playlists">Playlists</Link>
+      <Link to="/profile"
+        className={`item ${isActive('/profile')}`}><img src="../images/home.png" alt="home" /></Link>
+      <Link to="/albums"
+        className={`item ${isActive('/albums')}`}><img src="../images/album.png" alt="album" /></Link>
+      <Link to="/my-top"
+        className={`item ${isActive('/my-top')}`}><img src="../images/medal.png" alt="medal" /></Link>
+      <Link to="/playlists"
+        className={`item ${isActive('/playlists')}`}><img src="../images/playlist.png" alt="playlist" /></Link>
     </div>
   )
 }
 
-export default Menu;
+export default withRouter(Menu);
