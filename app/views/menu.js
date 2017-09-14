dd0dc2e88dd69ccf3ddde045c8fcf37ad607350f
leomunozr <leonardomr1@gmail.com>
2017-09-14 17:54:07 -0500

    load images from webpack

--- app/views/menu.js
+++ app/views/menu.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
-
+import homeImg from '../../images/home.png';
+import albumImg from '../../images/album.png';
+import medalImg from '../../images/medal.png';
+import playlistImg from '../../images/playlist.png';
+import exitImg from '../../images/exit.png';
 
 const Menu = (props) => {
 
@@ -13,27 +17,27 @@ const Menu = (props) => {
       <Link to="/profile"
         title="Profile"
         className={`item ${isActive('/profile')}`}>
-        <img src="../images/home.png" alt="home" />
+        <img src={homeImg} alt="home" />
       </Link>
       <Link to="/albums"
         title="Albums"
         className={`item ${isActive('/albums')}`}>
-        <img src="../images/album.png" alt="album" />
+        <img src={albumImg} alt="album" />
       </Link>
       <Link to="/my-top"
         title="My top"
         className={`item ${isActive('/my-top')}`}>
-        <img src="../images/medal.png" alt="medal" />
+        <img src={medalImg} alt="medal" />
       </Link>
       <Link to="/playlists"
         title="Playlists"
         className={`item ${isActive('/playlists')}`}>
-        <img src="../images/playlist.png" alt="playlist" />
+        <img src={playlistImg} alt="playlist" />
       </Link>
       <div className="item item--bottom"
         title="Logout"
         onClick={props.logout}>
-        <img src="../images/exit.png" alt="logout" />
+        <img src={exitImg} alt="logout" />
       </div>
     </div>
   )
